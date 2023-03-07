9b823a37959ef3764afc9bcd264abe7a1fdbdde7
Luiz Assano <luizaugusto1974@hotmail.com>
2023-03-07 20:05:02 -0300

    fix/solution2

--- teste de lógica/index.js
+++ teste de lógica/index.js
@@ -10,27 +10,6 @@ function solucao1(A) {
   
 
 //EX 02
-
-function solucao2(A) {
-  if (A.length === 0) {
-      return 0;
-  }
-  if (A.length === 1) {
-      return 1;
-  }
-  A.sort(function(a, b) {return a - b;});
-  let contador = 1;
-  let ultimoElemento = A[0];
-  for (let i = 1; i < A.length; i++) {
-      if (A[i] !== ultimoElemento) {
-          contador++;
-          ultimoElemento = A[i];
-      }
-  }
-  return contador;
-}
-
-
 function solucao2(arr) {
   const valoresUnicos = {}; 
   for (let i = 0; i < arr.length; i++) {
